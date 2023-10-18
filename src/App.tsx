f880bc060a210ae3004f06d1ed7169c62fcd9d24
Sudharsan S <sudharsanvsr@gmail.com>
2023-10-18 17:26:34 +0530

    Context functionalities done

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Routing from "./route";
 import { Navbar } from "./components/navBar";
+import { ShoppingCartProvider } from "./context/shoppingCartContext";
 
 export default function App() {
   return (
     <>
-    <Navbar />
-    <Container className="mb-4">
-      <Routing />
-    </Container>
+      <ShoppingCartProvider>
+        <Navbar />
+        <Container className="mb-4">
+          <Routing />
+        </Container>
+      </ShoppingCartProvider>
     </>
-
   );
 }
--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import storeItems from "../../data/items.json";
+import {Row, Col} from "react-bootstrap";
+import { StoreItems } from "../../components/storeItmes";
 
 const Store = () => {
     return (
@@ -6,6 +9,13 @@ const Store = () => {
         <h1>
             Store
         </h1>
+        <Row className="g-3" md ={3} xs={1} l={4}>
+            {storeItems.map(item => (
+                <Col>
+                <StoreItems {...item} />
+                </Col>
+            ))}
+        </Row>
         </>
     )
 }
